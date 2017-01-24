3b3c49cb071df13397253bf75f897577069f2975
Wei Sun <wsun@CNwsun.local>
2017-01-24 15:59:48 +0800

    add ansible and other task; Wayde

--- entry_point.sh
+++ entry_point.sh
@@ -2,7 +2,10 @@
 
 echo 'Ready. Go...'
 
-echo 'installing tools'
+echo 'installing tools...'
 ./scripts/install_tools.sh
+
+echo 'Packer docker image...'
+./packer_0.11.0/packer build -var "secret_key=wayde191" docker_packer_config.json
 
 echo 'Done!'
--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -9,5 +9,3 @@ if [ ! -d "./tools/packer_0.11.0" ]; then
     wget -O ./tools/packer_0.11.0.zip https://releases.hashicorp.com/packer/0.11.0/packer_0.11.0_linux_amd64.zip
     unzip ./tools/packer_0.11.0.zip -d ./tools/packer_0.11.0/
 fi
-
-#./packer_0.11.0/packer build -var "secret_key=$SECRET_KEY" -var "access_key=$ACCESS_KEY" -var "ecr_login_server=$ECR_LOGIN_SERVER" -var "ecr_repository=$ECR_REPOSITORY" docker_packer_config.json
