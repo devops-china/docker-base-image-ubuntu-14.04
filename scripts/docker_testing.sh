c445b57f5effccf8c1c2d4be66a52067c4185689
Wei Sun <wsun@CNwsun.local>
2017-01-24 18:04:33 +0800

    docker command; Wayde

--- scripts/docker_testing.sh
+++ scripts/docker_testing.sh
@@ -2,3 +2,7 @@
 
 docker run --name wayde -itd ihakula/ubuntu1404-ruby bash
 docker attach wayde
+docker ps
+docker run -di ihakula/ubuntu1404-ruby
+docker container stop $(docker ps -aq)
+docker rm $(docker ps -aq)
