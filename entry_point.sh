4b064025ff84e4a370f36fc96f09112ffc133269
Wei Sun <wsun@CNwsun.local>
2017-01-24 11:27:43 +0800

    reorgnized folder; Wayde

--- entry_point.sh
+++ entry_point.sh
@@ -3,17 +3,6 @@
 echo 'Ready. Go...'
 
 echo 'installing tools'
-
-apt-get update \
-&&  apt-get install -y unzip \
-&&  apt-get install -y ca-certificates \
-&&  apt-get install -y wget
-
-if [ ! -d "./packer_0.11.0" ]; then
-    wget -O ./packer_0.11.0.zip https://releases.hashicorp.com/packer/0.11.0/packer_0.11.0_linux_amd64.zip
-    unzip ./packer_0.11.0.zip -d ./packer_0.11.0/
-fi
-
-#./packer_0.11.0/packer build -var "secret_key=$SECRET_KEY" -var "access_key=$ACCESS_KEY" -var "ecr_login_server=$ECR_LOGIN_SERVER" -var "ecr_repository=$ECR_REPOSITORY" docker_packer_config.json
+./scripts/install_tools.sh
 
 echo 'Done!'
---
+++ scripts/install_tools.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+apt-get update \
+&&  apt-get install -y unzip \
+&&  apt-get install -y ca-certificates \
+&&  apt-get install -y wget
+
+if [ ! -d "../tools/packer_0.11.0" ]; then
+    wget -O ../tools/packer_0.11.0.zip https://releases.hashicorp.com/packer/0.11.0/packer_0.11.0_linux_amd64.zip
+    unzip ../tools/packer_0.11.0.zip -d ../tools/packer_0.11.0/
+fi
+
+#./packer_0.11.0/packer build -var "secret_key=$SECRET_KEY" -var "access_key=$ACCESS_KEY" -var "ecr_login_server=$ECR_LOGIN_SERVER" -var "ecr_repository=$ECR_REPOSITORY" docker_packer_config.json
