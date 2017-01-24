27f45861e669e68c2387d97533c5836d02031ff5
Wei Sun <wsun@CNwsun.local>
2017-01-24 11:21:49 +0800

    get tools ready; Wayde

--- entry_point.sh
+++ entry_point.sh
@@ -2,7 +2,18 @@
 
 echo 'Ready. Go...'
 
-echo 'installing ansible'
-./scripts/install_ansible.sh
+echo 'installing tools'
+
+apt-get update \
+&&  apt-get install -y unzip \
+&&  apt-get install -y ca-certificates \
+&&  apt-get install -y wget
+
+if [ ! -d "./packer_0.11.0" ]; then
+    wget -O ./packer_0.11.0.zip https://releases.hashicorp.com/packer/0.11.0/packer_0.11.0_linux_amd64.zip
+    unzip ./packer_0.11.0.zip -d ./packer_0.11.0/
+fi
+
+#./packer_0.11.0/packer build -var "secret_key=$SECRET_KEY" -var "access_key=$ACCESS_KEY" -var "ecr_login_server=$ECR_LOGIN_SERVER" -var "ecr_repository=$ECR_REPOSITORY" docker_packer_config.json
 
 echo 'Done!'
