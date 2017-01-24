30252abfe2fe15056ef7955b0bf5d002a1c42e3e
Wei Sun <wsun@CNwsun.local>
2017-01-24 16:01:32 +0800

    packer location; Wayde

--- entry_point.sh
+++ entry_point.sh
@@ -6,6 +6,6 @@ echo 'installing tools...'
 ./scripts/install_tools.sh
 
 echo 'Packer docker image...'
-./packer_0.11.0/packer build -var "secret_key=wayde191" docker_packer_config.json
+./tools/packer_0.11.0/packer build -var "secret_key=wayde191" docker_packer_config.json
 
 echo 'Done!'
