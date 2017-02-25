a2f9fe3e5544b057e6dc149fc0be6aaeef07a8c8
Wei Sun <wsun@CNwsun.local>
2017-02-25 22:22:34 +0800

    add ssh ; Wayde

--- scripts/docker_testing.sh
+++ scripts/docker_testing.sh
@@ -10,7 +10,7 @@ docker rm $(docker ps -aq)
 # Apache PHP
 docker run --name json-editor-online -p 8701:80 -e VIRTUAL_HOST=www.jsoneditoronline.cn -v /home/repos/jsoneditoronline/:/var/www/html/ -d eboraas/apache-php
 # source
-docker run --name zm-source -p 8702:80 -p 443:443 -e VIRTUAL_HOST=source.sunzhongmou.com -v /home/repos/source/:/var/www/html/ -d eboraas/apache-php
+docker run --name zm-source -p 8702:80 -e VIRTUAL_HOST=source.sunzhongmou.com -v /home/repos/source/:/var/www/html/ -d eboraas/apache-php
 
 # MySQL
 docker run --name zm-mysql -e MYSQL_ROOT_PASSWORD=Wayde191! -d mysql:5.6
@@ -24,7 +24,7 @@ docker inspect zm-mysql | grep IPAddress
 docker run --name zm-phpmyadmin -d --link zm-mysql:db -e VIRTUAL_HOST=phpmyadmin.sunzhongmou.com -p 8601:80 phpmyadmin/phpmyadmin
 
 # Nginx
-docker run --name zm-nginx -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
+docker run --name zm-nginx -d -p 80:80 -p 443:443 -v /home/repos/szm-cer:/etc/nginx/certs -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
 
 # wordpress
 docker run --name zm-wordpress --link zm-mysql:mysql -e WORDPRESS_DB_NAME=szm-blog -e VIRTUAL_HOST=blog.sunzhongmou.com -p 8501:80 -d wordpress
